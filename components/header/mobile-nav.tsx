659a77b1984d6c01c1aefa00075fa7298c2071ee
cynicalight <cynicalight@users.noreply.github.com>
2026-09-03 19:10:03 +0800

    fix: add mobile navigation sidebar

--- components/header/mobile-nav.tsx
+++ components/header/mobile-nav.tsx
@@ -4,6 +4,7 @@ import { clearAllBodyScrollLocks, disableBodyScroll, enableBodyScroll } from 'bo
 import { clsx } from 'clsx'
 import { Menu, X } from 'lucide-react'
 import { useEffect, useRef, useState } from 'react'
+import { createPortal } from 'react-dom'
 import { Link } from '~/components/ui/link'
 import { Twemoji } from '~/components/ui/twemoji'
 import { HEADER_NAV_LINKS, MORE_NAV_LINKS } from '~/data/navigation'
@@ -52,41 +53,42 @@ export function MobileNav() {
           <Menu size={22} />
         </button>
       </div>
-      {navShow && (
-        <div className="fixed inset-0 z-60 sm:hidden" role="dialog" aria-modal="true">
-          <button
-            type="button"
-            aria-label="Close Menu Backdrop"
-            className="fixed inset-0 bg-black/25"
-            onClick={onToggleNav}
-          />
-          <div className="fixed left-0 top-0 z-70 h-full w-full bg-white opacity-90 duration-300 dark:bg-gray-950 dark:opacity-[0.98]">
-            <nav
-              ref={navRef}
-              className="rounded-20 mt-4 flex flex-col items-start gap-4 overflow-y-auto pl-10 pt-2"
-            >
-              {[...HEADER_NAV_LINKS, ...MORE_NAV_LINKS].map((link) => (
-                <Link
-                  key={link.title}
-                  href={link.href}
-                  className="py-1 text-xl font-bold tracking-widest text-gray-900 outline outline-0 hover:text-primary-500 dark:text-gray-100 dark:hover:text-primary-400"
-                  onClick={onToggleNav}
-                >
-                  <Twemoji emoji={link.emoji} />
-                  <span className="ml-2">{link.title}</span>
-                </Link>
-              ))}
-            </nav>
+      {navShow &&
+        createPortal(
+          <div className="fixed inset-0 z-60 sm:hidden" role="dialog" aria-modal="true">
+            <div className="fixed inset-y-0 left-0 z-70 w-[90vw] bg-white dark:bg-gray-950">
+              <nav ref={navRef} className="h-full overflow-y-auto pb-8 pl-10 pr-20 pt-6">
+                <div className="flex flex-col items-start gap-4">
+                  {[...HEADER_NAV_LINKS, ...MORE_NAV_LINKS].map((link) => (
+                    <Link
+                      key={link.title}
+                      href={link.href}
+                      className="py-1 text-xl font-bold tracking-widest text-gray-900 outline outline-0 hover:text-primary-500 dark:text-gray-100 dark:hover:text-primary-400"
+                      onClick={onToggleNav}
+                    >
+                      <Twemoji emoji={link.emoji} />
+                      <span className="ml-2">{link.title}</span>
+                    </Link>
+                  ))}
+                </div>
+              </nav>
+              <button
+                className="absolute right-4 top-5 z-80 h-16 w-16 p-4 text-gray-900 hover:text-primary-500 dark:text-gray-100 dark:hover:text-primary-400"
+                aria-label="Toggle Menu"
+                onClick={onToggleNav}
+              >
+                <X className="h-7 w-7" strokeWidth={1.5} />
+              </button>
+            </div>
             <button
-              className="fixed right-4 top-5 z-80 h-16 w-16 p-4 text-gray-900 hover:text-primary-500 dark:text-gray-100 dark:hover:text-primary-400"
-              aria-label="Toggle Menu"
+              type="button"
+              aria-label="Close Menu Backdrop"
+              className="fixed inset-y-0 right-0 w-[10vw] bg-black/15 backdrop-blur-md"
               onClick={onToggleNav}
-            >
-              <X className="h-7 w-7" strokeWidth={1.5} />
-            </button>
-          </div>
-        </div>
-      )}
+            />
+          </div>,
+          document.body
+        )}
     </>
   )
 }
